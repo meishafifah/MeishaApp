97fd201a7122e5226f22646425c66e4321fc4bf6
meishafifah <meishafifah@students.amikom.ac.id>
2023-10-04 17:08:40 +0700

    feat: add fragment view

--- app/src/main/java/com/example/meishaapp/HomeActivity.kt
+++ app/src/main/java/com/example/meishaapp/HomeActivity.kt
@@ -1,11 +1,21 @@
 package com.example.meishaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.meishaapp.databinding.ActivityBookKidBinding
+import com.example.meishaapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        lateinit var binding: ActivityHomeBinding
+        binding.buttonMenu1.setOnClickListener {
+            val i = Intent(applicationContext, ActivityBookKidBinding::class.java)
+            startActivity(i)
+        }
+
     }
 }
