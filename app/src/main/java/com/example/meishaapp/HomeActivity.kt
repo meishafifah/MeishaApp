f2cbec258dfa5175c3684560471c6b2dcc16721c
meishafifah <meishafifah@students.amikom.ac.id>
2023-10-11 16:55:23 +0700

    feat: recycle view

--- app/src/main/java/com/example/meishaapp/HomeActivity.kt
+++ app/src/main/java/com/example/meishaapp/HomeActivity.kt
@@ -3,8 +3,11 @@ package com.example.meishaapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.meishaapp.databinding.ActivityBookKidBinding
 import com.example.meishaapp.databinding.ActivityHomeBinding
+import java.util.ArrayList
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,15 @@ class HomeActivity : AppCompatActivity() {
             startActivity(i)
         }
 
+        val rvBuku:RecyclerView = binding.recyclerViewBuku
+        rvBuku.layoutManager = LinearLayoutManager(this)
+
+        val data = ArrayList<BukuModel>()
+        data.add(BukuModel(R.drawable.book1, "Emi's Beach Adventure", "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"))
+        data.add(BukuModel(R.drawable.book2, "Ade's Beach Adventure", "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"))
+        data.add(BukuModel(R.drawable.book4, "Mermaid to rescue", "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"))
+
+        val adapter = AdapterHome(data)
+        rvBuku.adapter = adapter
     }
 }
